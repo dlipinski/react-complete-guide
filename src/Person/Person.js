bce450cfb2c47542cb7862e2c5bd189c7128c126
Dawid Lipiński <dawidlipinski@Dawids-MacBook-Pro.local>
2020-05-18 23:52:26 +0200

    38. Understanding & Using State

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const person = (props) => {
+const person = ( props ) => {
     return (
         <div>
             <p>I'm a {props.name} and I am {props.age} years old!</p>
