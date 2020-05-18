042c20d5253dac1d6cfe5fa8a1ced9b6723840b3
Dawid Lipiński <dawidlipinski@Dawids-MacBook-Pro.local>
2020-05-18 23:25:16 +0200

    done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi, i'm a react app</h1>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, i'm a react app</h1>
+    //   </div>
+    // );
+
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m React App!!'))
   }
 }
 
