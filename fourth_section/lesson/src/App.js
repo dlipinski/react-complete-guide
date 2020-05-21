fbf63e01606233e3ade25d86690334f2a6205e93
Dawid Lipiński <dawidlipinski@Dawids-MacBook-Pro.local>
2020-05-21 21:46:49 +0200

    54. Handling Dynamic Content "The JavaScript Way"

--- fourth_section/lesson/src/App.js
+++ fourth_section/lesson/src/App.js
@@ -49,6 +49,26 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}/>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Max!')}
+            changed={this.nameChangedHandler}> Hello </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}/>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, i'm a react app</h1>
@@ -56,21 +76,7 @@ class App extends Component {
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        { this.state.showPersons === true ?
-            <div>
-              <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}/>
-              <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Max!')}
-                changed={this.nameChangedHandler}> Hello </Person>
-              <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}/>
-            </div> : null
-        }
+        {persons}
       </div>
     );
     // () => funcs less efficent than bind()
